d826bcdc855ec8d6cbace231eee90b4bf1f23ea3
Andrei Lisnic <andrei.lisnic@gmail.com>
2020-07-12 17:43:40 +0300

    Moving squares!

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import ResourceManager from "./lib/resource_manager.js";
 import ShaderLoader from "./lib/shader_loader.js";
 import Program from "./lib/program.js";
 import Renderer from "./lib/renderer.js";
+import Entity from "./lib/entity.js";
+import Mover from "./lib/mover.js";
 
 var manager = new ResourceManager({
   point_vertex: "shaders/point.vert",
@@ -36,45 +38,49 @@ export default function run(gl, tickFn) {
     var vertexShader = loader.loadVertex(manager.data.point_vertex);
     var fragmentShader = loader.loadFragment(manager.data.point_fragment);
 
-    var program = new Program(gl)
-      .attachShader(vertexShader)
-      .attachShader(fragmentShader);
+    var program = new Program(gl).attachShader(vertexShader).attachShader(fragmentShader);
 
     var shaderProg = program.compile();
 
     var aPositionLoc = program.getAttribLocation("a_position"),
       uPointSizeLoc = program.getUniformLocation("uPointSize");
 
-    //............................................
-    //Set Up Data Buffers
-    var aryVerts = new Float32Array([0, 0, 0, 0.5, 0.5, 0]),
-      bufVerts = gl.createBuffer();
+    var square1 = new Entity("square1", { x: Math.random(), y: Math.random() });
+    var square2 = new Entity("square2", { x: Math.random(), y: Math.random() });
+
+    var aryVerts = new Float32Array([
+      square1.data.x,
+      square1.data.y,
+      0,
+      square2.data.x,
+      square2.data.y,
+      0,
+    ]);
+
+    var bufVerts = gl.createBuffer();
 
     gl.bindBuffer(gl.ARRAY_BUFFER, bufVerts);
     gl.bufferData(gl.ARRAY_BUFFER, aryVerts, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-    //............................................
-    //Set Up For Drawing
-    gl.useProgram(shaderProg); //Activate the Shader
-    gl.uniform1f(uPointSizeLoc, 50.0); //Store data to the shader's uniform variable uPointSize
+    gl.useProgram(shaderProg);
+    gl.uniform1f(uPointSizeLoc, 50.0);
 
-    //how its down without VAOs
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufVerts); //Tell gl which buffer we want to use at the moment
-    gl.enableVertexAttribArray(aPositionLoc); //Enable the position attribute in the shader
-    gl.vertexAttribPointer(aPositionLoc, 3, gl.FLOAT, false, 0, 0); //Set which buffer the attribute will pull its data from
-    gl.bindBuffer(gl.ARRAY_BUFFER, null); //Done setting up the buffer
+    gl.enableVertexAttribArray(aPositionLoc);
+    gl.vertexAttribPointer(aPositionLoc, 3, gl.FLOAT, false, 0, 0);
 
-    gl.drawArrays(gl.POINTS, 0, 2); //Draw the points
+    var mover = new Mover().register(square1).register(square2);
 
     new Renderer({ fps: 30 }).render(() => {
-      console.log("RENDER!");
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      aryVerts[0] = Math.random();
-      aryVerts[1] = Math.random();
-      aryVerts[3] = Math.random();
-      aryVerts[4] = Math.random();
-      gl.drawArrays(gl.POINTS, 0, 2); //Draw the points
+      mover.commit();
+
+      aryVerts[0] = square1.data.x;
+      aryVerts[1] = square1.data.y;
+      aryVerts[3] = square2.data.x;
+      aryVerts[4] = square2.data.y;
+
+      gl.bufferData(gl.ARRAY_BUFFER, aryVerts, gl.DYNAMIC_DRAW);
+      gl.drawArrays(gl.POINTS, 0, 2);
     });
   });
 }
