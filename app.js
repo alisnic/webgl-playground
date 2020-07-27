9c50d6fe3eac3340a3a5afd8e0c797ad9be7f0e7
Andrei Lisnic <andrei.lisnic@gmail.com>
2020-07-27 12:36:09 +0300

    make mesh generic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import Program from "./lib/program.js";
 import Renderer from "./lib/renderer.js";
 import Grid from "./src/grid.js";
 import { vec4 } from "https://unpkg.com/gl-matrix@3.3.0/esm/index.js";
+import Mesh from "./lib/Mesh.js";
 
 var manager = new ResourceManager({
   point_vertex: "shaders/point.vert",
@@ -43,11 +44,29 @@ export default function run(gl) {
       .activate()
       .set("uColor", [0.8, 0.8, 0.8, 1, 0, 0, 0, 1, 0, 0, 0, 1]);
 
-    var grid = new Grid(program);
+    // var grid = new Grid(program);
+    var grid = new Mesh(program, gl.LINES)
+      .setData(Grid.build())
+      .setComponentSize(4)
+      .enableAttributes({
+        a_position: {
+          size: 3,
+          type: "FLOAT",
+          stride: Float32Array.BYTES_PER_ELEMENT * 4,
+        },
+        a_color: {
+          size: 1,
+          type: "FLOAT",
+          stride: Float32Array.BYTES_PER_ELEMENT * 4,
+          offset: Float32Array.BYTES_PER_ELEMENT * 3,
+        },
+      });
 
     new Renderer({ fps: 0 }).render((dt) => {
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      grid.render();
+      // grid.render();
+      var vertexCount = grid.dataSize / grid.componentSize;
+      gl.drawArrays(gl.LINES, 0, vertexCount);
     });
   });
 }
--- src/grid.js
+++ src/grid.js
@@ -1,33 +1,5 @@
-import Mesh from "../lib/Mesh.js";
-
-export default class Grid extends Mesh {
-  static ATTRIBUTES = {
-    a_position: {
-      size: 3,
-      type: "FLOAT",
-      stride: Float32Array.BYTES_PER_ELEMENT * 4,
-    },
-    a_color: {
-      size: 1,
-      type: "FLOAT",
-      stride: Float32Array.BYTES_PER_ELEMENT * 4,
-      offset: Float32Array.BYTES_PER_ELEMENT * 3,
-    },
-  };
-
-  constructor(program) {
-    super(program);
-    this.verts = this.makeData();
-    this.setData(this.verts);
-    this.enableAttributes(Grid.ATTRIBUTES);
-  }
-
-  render() {
-    var vertexCount = this.verts.length / 4;
-    this.gl.drawArrays(this.gl.LINES, 0, vertexCount);
-  }
-
-  makeData() {
+export default class Grid {
+  static build() {
     var verts = [],
       size = 1.8, // W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
       div = 10.0, // How to divide up the grid
