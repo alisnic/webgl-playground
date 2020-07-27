07ebf614a9bee8a3e53154394d0a6fe0f78c234f
Andrei Lisnic <andrei.lisnic@gmail.com>
2020-07-27 14:46:01 +0300

    extract canvas resize

--- app.js
+++ app.js
@@ -3,27 +3,14 @@ import Grid from "./src/grid.js";
 import GridShader from "./src/GridShader.js";
 import Camera from "./lib/Camera.js";
 import CameraController from "./lib/CameraController.js";
-
-/**
- * @param {WebGLRenderingContext} gl - WebGL instance
- * @param {number} width
- * @param {number} height
- */
-function setCanvasSize(gl, width, height) {
-  gl.canvas.style.width = width + "px";
-  gl.canvas.style.height = height + "px";
-  gl.canvas.width = width;
-  gl.canvas.height = height;
-
-  gl.viewport(0, 0, width, height);
-}
+import Platform from "./lib/Platform.js";
 
 /**
  * @param {WebGLRenderingContext} gl - WebGL instance
  */
 export default function run(gl) {
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
-  setCanvasSize(gl, innerWidth, innerHeight);
+  Platform.setCanvasSize(gl, innerWidth, innerHeight);
 
   var gCamera = new Camera(gl);
   gCamera.transform.position.set(0, 1, 3);
