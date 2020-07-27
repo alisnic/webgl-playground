c28f1a77c4ce2e6d08ca12ef1ac3d00a5e166dbe
Andrei Lisnic <andrei.lisnic@gmail.com>
2020-07-27 09:36:08 +0300

    stuff works

--- app.js
+++ app.js
@@ -36,35 +36,91 @@ export default function run(gl) {
     var fragmentShader = loader.loadFragment(manager.data.point_fragment);
 
     var program = new Program(gl)
-      .uniforms({ uPointSize: "uniform1f", uAngle: "uniform1f" })
+      .uniforms({ uColor: "uniform3fv" })
       .attachShader(vertexShader)
       .attachShader(fragmentShader)
       .activate();
 
+    var verts = gridData();
     var buffer = new GpuBuffer(gl, gl.ARRAY_BUFFER)
       .activate()
-      .loadData(
-        [0, 0, 0, 0.1, 0.1, 0, 0.1, -0.1, 0, -0.1, -0.1, 0, -0.1, 0.1, 0],
-        gl.STATIC_DRAW
-      );
+      .loadData(verts, gl.STATIC_DRAW);
 
-    program.set("uPointSize", 50.0);
-    program.enableVertexArray("a_position", 3, gl.FLOAT);
+    program.set("uColor", [0.8, 0.8, 0.8, 1, 0, 0, 0, 1, 0, 0, 0, 1]);
 
-    var gPointSize = 0,
-      gPSizeStep = 3,
-      gAngle = 0,
-      gAngleStep = (Math.PI / 180.0) * 90; //90 degrees in Radians
+    var strideLen = Float32Array.BYTES_PER_ELEMENT * 4;
+    program.enableVertexArray("a_position", 3, gl.FLOAT, strideLen);
+    program.enableVertexArray(
+      "a_color",
+      1,
+      gl.FLOAT,
+      strideLen,
+      Float32Array.BYTES_PER_ELEMENT * 3
+    );
 
-    new Renderer({ fps: 60 }).render((dt) => {
-      gPointSize += (gPSizeStep * dt) / 1000;
-      var size = Math.sin(gPointSize) * 10.0 + 30.0;
-      program.set("uPointSize", size);
+    var vertexCount = verts.length / 4;
 
-      gAngle += (gAngleStep * dt) / 1000;
-      program.set("uAngle", gAngle);
+    new Renderer({ fps: 0 }).render((dt) => {
       gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-      gl.drawArrays(gl.POINTS, 0, 5);
+      gl.drawArrays(gl.LINES, 0, vertexCount);
     });
   });
+}
+
+function gridData() {
+  var verts = [],
+    size = 1.8, // W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
+    div = 10.0, // How to divide up the grid
+    step = size / div, // Steps between each line, just a number we increment by for each line in the grid.
+    half = size / 2; // From origin the starting position is half the size.
+
+  var p; //Temp variable for position value.
+  for (var i = 0; i <= div; i++) {
+    //Vertical line
+    p = -half + i * step;
+    verts.push(p); //x1
+    verts.push(half); //y1
+    verts.push(0); //z1
+    verts.push(0); //c2
+
+    verts.push(p); //x2
+    verts.push(-half); //y2
+    verts.push(0); //z2
+    verts.push(1); //c2
+
+    //Horizontal line
+    p = half - i * step;
+    verts.push(-half); //x1
+    verts.push(p); //y1
+    verts.push(0); //z1
+    verts.push(0); //c1
+
+    verts.push(half); //x2
+    verts.push(p); //y2
+    verts.push(0); //z2
+    verts.push(1); //c2
+  }
+
+  //TODO : Remove the following, its only to demo extra lines can be thrown in.
+  verts.push(-half); //x1
+  verts.push(-half); //y1
+  verts.push(0); //z1
+  verts.push(2); //c2
+
+  verts.push(half); //x2
+  verts.push(half); //y2
+  verts.push(0); //z2
+  verts.push(2); //c2
+
+  verts.push(-half); //x1
+  verts.push(half); //y1
+  verts.push(0); //z1
+  verts.push(3); //c2
+
+  verts.push(half); //x2
+  verts.push(-half); //y2
+  verts.push(0); //z2
+  verts.push(3); //c2
+
+  return verts;
 }
