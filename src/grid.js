cf8689f358d3976d444de06bdd9a752477eb6b00
Andrei Lisnic <andrei.lisnic@gmail.com>
2020-07-28 10:46:31 +0300

    use grid data from tutorial

--- src/grid.js
+++ src/grid.js
@@ -25,36 +25,36 @@ export default class Grid {
 
   static build(incAxis) {
     var verts = [],
-      size = 1.8, // W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
+      size = 2, // W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
       div = 10.0, // How to divide up the grid
       step = size / div, // Steps between each line, just a number we increment by for each line in the grid.
-      half = size / 2; // From origin the starting position is half the size.
+      half = size / 2;
 
     var p; //Temp variable for position value.
     for (var i = 0; i <= div; i++) {
       //Vertical line
       p = -half + i * step;
       verts.push(p); //x1
-      verts.push(half); //y1
-      verts.push(0); //z1
+      verts.push(0); //y1 verts.push(half);
+      verts.push(half); //z1 verts.push(0);
       verts.push(0); //c2
 
       verts.push(p); //x2
-      verts.push(-half); //y2
-      verts.push(0); //z2
-      verts.push(1); //c2
+      verts.push(0); //y2 verts.push(-half);
+      verts.push(-half); //z2 verts.push(0);
+      verts.push(0); //c2 verts.push(1);
 
       //Horizontal line
       p = half - i * step;
       verts.push(-half); //x1
-      verts.push(p); //y1
-      verts.push(0); //z1
+      verts.push(0); //y1 verts.push(p);
+      verts.push(p); //z1 verts.push(0);
       verts.push(0); //c1
 
       verts.push(half); //x2
-      verts.push(p); //y2
-      verts.push(0); //z2
-      verts.push(1); //c2
+      verts.push(0); //y2 verts.push(p);
+      verts.push(p); //z2 verts.push(0);
+      verts.push(0); //c2 verts.push(1);
     }
 
     if (incAxis) {
